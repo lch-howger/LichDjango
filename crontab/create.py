8fa22817028a05aa06b7e44fa0265ecc7dd36550
lch-howger <360000521@qq.com>
2019-12-05 11:47:03 +0000

    update create database
    update create database

--- crontab/create.py
+++ crontab/create.py
@@ -2,19 +2,13 @@ import os
 import sqlite3
 
 sql_create_table = 'CREATE TABLE stars (id TEXT NOT NULL, star TEXT NOT NULL, time TEXT NOT NULL, text TEXT NOT NULL)'
-
-# connect to the database from filename
-file_exists = os.path.exists('./stars.db')
-if file_exists:
-    os.remove('./stars.db')
+filename_db = '../db.sqlite3'
 
 # connect to the database and get the cursor
-db = sqlite3.connect('./stars.db')
+db = sqlite3.connect(filename_db)
 cursor = db.cursor()
 
 # create database and table
 cursor.execute(sql_create_table)
 db.commit()
-
 cursor.close()
-db.close()
