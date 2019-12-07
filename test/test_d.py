3026eb36f53b88ad880c1b85275574ac3b580195
lch-howger <360000521@qq.com>
2019-12-07 17:27:12 +0000

    update test_cde

---
+++ test/test_d.py
@@ -0,0 +1,11 @@
+import time
+import os
+
+abspath = os.path.abspath('.')
+path = os.path.join(abspath, 'test.txt')
+
+str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+file = open(path, 'a')
+file.write('DDDDD------' + str + '\n')
+file.close()
