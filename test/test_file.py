098e1d56d8fc2391b9c8d98cbff358decf784a44
lch-howger <360000521@qq.com>
2019-12-09 21:49:17 +0000

    update test

--- test/test_file.py
+++ test/test_file.py
@@ -6,6 +6,12 @@ path = os.path.join(abspath, 'test.txt')
 
 str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
+print(path)
+
+file = open(path, 'a')
+file.write('AAA------' + path + '\n')
+file.close()
+
 file = open(path, 'a')
 file.write('AAA------' + str + '\n')
 file.close()
