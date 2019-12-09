8c4a37a60ee919070169d4b77eb89298ce2cc06a
lch-howger <360000521@qq.com>
2019-12-09 22:43:06 +0000

    update test

---
+++ test/test.py
@@ -0,0 +1,3 @@
+file = open('./test.txt', 'a')
+file.write('Hello World\n')
+file.close()
--- test/test_file.py
+++ test/test_file.py
@@ -1,17 +1,8 @@
 import time
 import os
 
-abspath = os.path.abspath('.')
-path = os.path.join(abspath, 'test.txt')
-
 str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-print(path)
-
-file = open(path, 'a')
-file.write('AAA------' + path + '\n')
-file.close()
-
-file = open(path, 'a')
+file = open('test.txt', 'a')
 file.write('AAA------' + str + '\n')
 file.close()
